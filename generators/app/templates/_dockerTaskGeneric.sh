5bf4104bc804994d12f5c64c7fad9dc683dcf35f
Nathan Carlson <NCarlson@microsoft.com>
2016-06-17 09:39:02 -0700

    Rebrand from ASP.NET to .NET (#71)

--- generators/app/templates/_dockerTaskGeneric.sh
+++ generators/app/templates/_dockerTaskGeneric.sh
@@ -1,5 +1,5 @@
 imageName="<%= imageName %>"
-projectName="<%= composeProjectName %>"<% if (projectType === 'aspnet') { %>
+projectName="<%= composeProjectName %>"<% if (projectType === 'dotnet') { %>
 serviceName="<%= serviceName %>"
 containerName="<%= '${projectName}_${serviceName}' %>_1"<% } %>
 publicPort=<%= portNumber %>
@@ -49,7 +49,7 @@ compose () {
     docker-compose -f $composeFileName -p $projectName kill
     docker-compose -f $composeFileName -p $projectName up -d
   fi
-}<% if (projectType === 'aspnet') { %>
+}<% if (projectType === 'dotnet') { %>
 
 startDebugging () {
     echo "Running on $url"
@@ -82,7 +82,7 @@ showUsage () {
     echo "Commands:"
     echo "    build: Builds a Docker image ('$imageName')."
     echo "    compose: Runs docker-compose."
-    echo "    clean: Removes the image '$imageName' and kills all containers based on that image."<% if (projectType === 'aspnet') { %>
+    echo "    clean: Removes the image '$imageName' and kills all containers based on that image."<% if (projectType === 'dotnet') { %>
     echo "    composeForDebug: Builds the image and runs docker-compose."
     echo "    startDebugging: Finds the running container and starts the debugger inside of it."<% } %>
     echo ""
@@ -107,7 +107,7 @@ else
              if [[ $isWebProject = true ]]; then
                openSite
              fi
-             ;;<% if (projectType === 'aspnet') { %>
+             ;;<% if (projectType === 'dotnet') { %>
       "composeForDebug")
              ENVIRONMENT=$2
              export REMOTE_DEBUGGING=1
